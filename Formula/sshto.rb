39dc4ccce5880b2224836cc5300676049f8eef59
Dexter Codo <5592994+dextercodo@users.noreply.github.com>
2025-12-14 08:33:00 +1100

    added sshto

---
+++ Formula/sshto.rb
@@ -0,0 +1,9 @@
+  # Placeholder - will be replaced by GoReleaser
+  class Sshto < Formula
+    desc "Placeholder"
+    homepage "https://github.com/codoworks/sshto"
+    version "0.0.0"
+
+    def install
+    end
+  end
